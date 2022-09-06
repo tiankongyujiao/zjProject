f8f86478f44adab90bf5454fe4d2b94ce955050e
zhaojiao <836409324@qq.com>
2022-09-06 09:02:20 +0800

    实现解析element+text功能

--- 源码学习/my-mini-vue/src/parse-core/src/parse.ts
+++ 源码学习/my-mini-vue/src/parse-core/src/parse.ts
@@ -1,4 +1,8 @@
 import { NodeTypes } from "../src/ast";
+const enum TagType {
+  Start,
+  End,
+}
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
@@ -13,11 +17,57 @@ function createRoot(children) {
 function parseChildren(context) {
   const nodes: any = [];
   let node;
-  if (context.source.startsWith("{{")) {
+  const s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
+  }
+  if (!node) {
+    node = parseText(context);
   }
   nodes.push(node);
   return nodes;
+}
+
+function parseText(context) {
+  const content = parseTextData(context, context.source.length);
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
+
+function parseTextData(context, length) {
+  const content = context.source.slice(0, length);
+  advanceBy(context, length);
+  return content;
+}
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.Start);
+
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context, type) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  console.log(context.source, "aaaaaaaaaaaaaaaaaa");
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
@@ -30,9 +80,9 @@ function parseInterpolation(context) {
   );
   advanceBy(context, openDelimiter.length);
   const rawContentLength = closeIndex - openDelimiter.length;
-  const rawContent = context.source.slice(0, rawContentLength);
+  const rawContent = parseTextData(context, rawContentLength);
   const content = rawContent.trim();
-  advanceBy(context, rawContentLength + closeDelimiter.length);
+  advanceBy(context, closeDelimiter.length);
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
