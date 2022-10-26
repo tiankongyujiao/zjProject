c22903f9f02f4ca6be843b15dac50c98ff01e569
zhaojiao <836409324@qq.com>
2022-10-26 08:55:18 +0800

    [add]实现 transform 功能

---
+++ 源码学习/my-mini-vue/src/parse-core/src/transform.ts
@@ -0,0 +1,30 @@
+export function transform(root: any, options) {
+  const context = createTransformContext(root, options);
+  traverseNode(root, context);
+}
+
+function createTransformContext(root, options) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+  return context;
+}
+
+function traverseNode(node, context) {
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    nodeTransforms[i](node);
+  }
+  traverseChildren(node, context);
+}
+
+function traverseChildren(node, context) {
+  const children = node.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const n = children[i];
+      traverseNode(n, context);
+    }
+  }
+}
