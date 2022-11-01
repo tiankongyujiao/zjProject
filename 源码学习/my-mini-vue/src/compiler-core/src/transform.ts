03d1d4d97835fbfbbd99f8c26a2adeae1a35f420
zhaojiao <jiao900414>
2022-11-01 17:54:09 +0800

    【实现代码生成插值类型】

--- 源码学习/my-mini-vue/src/compiler-core/src/transform.ts
+++ 源码学习/my-mini-vue/src/compiler-core/src/transform.ts
@@ -1,7 +1,12 @@
+import { NodeTypes } from "../../parse-core/src/ast";
+import { TO_DISPLAY_STRING } from "./runtimeHelpers";
+
 export function transform(root: any, options = {}) {
   const context = createTransformContext(root, options);
   traverseNode(root, context);
   createRootCodegen(root);
+
+  root.helpers = [...context.helpers.keys()];
 }
 
 function createRootCodegen(root) {
@@ -12,6 +17,10 @@ function createTransformContext(root, options) {
   const context = {
     root,
     nodeTransforms: options.nodeTransforms || [],
+    helpers: new Map(),
+    helper: (key) => {
+      context.helpers.set(key, 1);
+    },
   };
   return context;
 }
@@ -21,7 +30,18 @@ function traverseNode(node, context) {
   for (let i = 0; i < nodeTransforms.length; i++) {
     nodeTransforms[i](node);
   }
-  traverseChildren(node, context);
+  switch (node.type) {
+    case NodeTypes.INTERPOLATION:
+      context.helper(TO_DISPLAY_STRING);
+      break;
+
+    case NodeTypes.ROOT:
+    case NodeTypes.ELEMENT:
+      traverseChildren(node, context);
+      break;
+    default:
+      break;
+  }
 }
 // 处理children
 function traverseChildren(node, context) {
--- 源码学习/my-mini-vue/src/compiler-core/test/codegen.spec.ts
+++ 源码学习/my-mini-vue/src/compiler-core/test/codegen.spec.ts
@@ -1,11 +1,21 @@
 import { baseParse } from "../../parse-core/src/parse";
 import { generate } from "../src/codegen";
 import { transform } from "../src/transform";
+import { transformExpression } from "../src/transform/transformExpression";
 
 describe("codegen", () => {
   it("string", () => {
     const ast = baseParse("hi");
     transform(ast);
+    const { code } = generate(ast);
+    expect(code).toMatchSnapshot();
+  });
+
+  it("interpolation", () => {
+    const ast = baseParse("{{message}}");
+    transform(ast, {
+      nodeTransforms: [transformExpression],
+    });
     const { code } = generate(ast);
     expect(code).toMatchSnapshot();
   });
